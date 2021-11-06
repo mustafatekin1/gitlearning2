ae506dee022e770a61a852827c4451bcd890d2cd
mustafatekin1 <valid.testio@gmail.com>
2021-11-06 23:46:08 +0300

    3rd

--- src/FirstJavaClass.java
+++ src/FirstJavaClass.java
@@ -2,5 +2,9 @@ public class FirstJavaClass {
 
     public static void main(String[] args) {
         System.out.println("first push from Intellij to Github");
+        int x = 5;
+        int y= 2*x;
+        System.out.println(y);
+
     }
 }
